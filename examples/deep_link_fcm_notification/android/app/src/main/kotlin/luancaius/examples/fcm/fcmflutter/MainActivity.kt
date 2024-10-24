b446fbbf32556cd1a55a9b818011d349801675b8
luancaius <6966770+luancaius@users.noreply.github.com>
2024-10-23 23:26:12 -0600

    feat: adding web version

---
+++ examples/deep_link_fcm_notification/android/app/src/main/kotlin/luancaius/examples/fcm/fcmflutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package luancaius.examples.fcm.fcmflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- examples/deep_link_notification/android/app/src/main/kotlin/com/example/deep_link_notification/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.deep_link_notification
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
